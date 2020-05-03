ee02bf4609068f84de256391ed695abfdd0434f3
Darkrevenger-crypto <57855674+Darkrevenger-crypto@users.noreply.github.com>
2020-05-03 17:10:02 +0530

    Scroll effect

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,9 @@
 const menu = document.querySelector('.menu');
 const close = document.querySelector('.close');
 const dropdown = document.querySelector('.dropdown');
+let sval=window.scrollY;
+const h7=document.querySelector('.h7');
+const com=document.querySelector('.com');
 
 
 dropdown.style.display='none';
@@ -20,3 +23,17 @@ function Close(e)
     
 };
 
+window.addEventListener('scroll',Scroll);
+
+function Scroll(e)
+{
+if (sval>200)
+{
+ h7.style.visibility='hidden';
+ com.style.visibility='hidden';
+}
+else{
+    h7.style.visibility='visible';
+    com.style.visibility='visible';
+}
+};
